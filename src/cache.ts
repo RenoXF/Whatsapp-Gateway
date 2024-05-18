a4425ae92561ed4b39bc5d24fecf9611a8d9d037
Ashary Vermaysha <vermaysha@gmail.com>
2024-05-18 09:04:58 +0700

    feat: add multiple cache and redis cluster

--- src/cache.ts
+++ src/cache.ts
@@ -1,16 +1,41 @@
-import { createCache } from 'cache-manager'
+import { Cache, createCache, memoryStore, multiCaching } from 'cache-manager'
 import { redisInsStore } from 'cache-manager-ioredis-yet'
-import { Redis } from 'ioredis'
+import { Cluster, Redis } from 'ioredis'
 
-const redis = new Redis(process.env.REDIS_URL ?? '', {
-  reconnectOnError: () => true,
-  maxRetriesPerRequest: null,
-})
+let redis: Redis | Cluster | null = null
 
-console.log(`Redis URL: ${process.env.REDIS_URL}`)
+if (process.env.REDIS_URL && process.env.REDIS_URL_2) {
+  console.log(`Redis URL: ${process.env.REDIS_URL}`)
+  console.log(`Redis URL 2: ${process.env.REDIS_URL_2}`)
+  redis = new Cluster([process.env.REDIS_URL, process.env.REDIS_URL_2], {})
+} else if (process.env.REDIS_URL && !process.env.REDIS_URL_2) {
+  console.log(`Redis URL: ${process.env.REDIS_URL}`)
+  redis = new Redis(process.env.REDIS_URL ?? '', {
+    reconnectOnError: () => true,
+    maxRetriesPerRequest: null,
+  })
+}
 
-export const cache = createCache(
-  redisInsStore(redis, {
-    ttl: 4 * 24 * 60 * 60 * 1000, // 4 days
+const ttl = 345600000
+
+const memCache = createCache(
+  memoryStore({
+    ttl,
   }),
 )
+
+const caches: Cache[] = [memCache]
+
+if (redis) {
+  const redisCache = createCache(
+    redisInsStore(redis, {
+      ttl,
+    }),
+    {
+      ttl,
+    },
+  )
+  caches.push(redisCache)
+}
+
+export const cache = multiCaching(caches)
